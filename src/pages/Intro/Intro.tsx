8de2e371f7adcf7366c0d80139911a3cbc678da1
Anudeep.muktinutalapati <anudeep.muktinutalapati@skypointcloud.com>
2023-03-20 22:41:34 +0530

    Updated images

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -9,7 +9,7 @@ export default function Intro() {
         <div className="outline" />
         <div className="heading-hi">hey! I'm</div>
         <div className="heading-name">Anudeep</div>
-        <div className="heading-desc">a web dev building value by design</div>
+        <div className="heading-desc">a web dev, building value by design</div>
         <div className="outline" />
       </div>
       <div className="intro-image-container">
