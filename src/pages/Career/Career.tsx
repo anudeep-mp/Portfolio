11846167c9d698cf0d3618da3dab2799d934c35d
Anudeep.muktinutalapati <anudeep.muktinutalapati@skypointcloud.com>
2023-03-23 00:27:11 +0530

    Updated career page

--- src/pages/Career/Career.tsx
+++ src/pages/Career/Career.tsx
@@ -5,77 +5,62 @@ import VitLogo from "../../assets/images/vit-logo.png";
 import "./career.css";
 
 export default function Career() {
+  const carrerData = [
+    {
+      id: 1,
+      orgLogo: VitLogo,
+      orgName: "Vellore Institute of Technology",
+      orgPosition: "Bachelor of Technology",
+      orgTenure: "July 2017 - June 2021",
+      orgLink: "https://vit.ac.in",
+    },
+    {
+      id: 2,
+      orgLogo: OneBancLogo,
+      orgName: "OneBanc Technologies",
+      orgPosition: "Associate Frontend Developer",
+      orgTenure: "Oct 2021 - Feb 2022",
+      orgLink: "https://onebanc.ai",
+    },
+    {
+      id: 3,
+      orgLogo: OneBancLogo,
+      orgName: "OneBanc Technologies",
+      orgPosition: "Frontend Developer",
+      orgTenure: "Feb 2022 - June 2022",
+      orgLink: "https://onebanc.ai",
+    },
+    {
+      id: 4,
+      orgLogo: SpcLogo,
+      orgName: "SkyPoint Cloud",
+      orgPosition: "Software Development Engineer - I",
+      orgTenure: "June 2022 - Present",
+      orgLink: "https://skypointcloud.com",
+    },
+  ];
+
   return (
     <div className="section career">
       <div className="title">Career</div>
       <div className="content">
         <ul className="step-progress">
-          <li className="step-progress-item">
-            <div className="step-progress-item-content">
-              <img src={SpcLogo} className="org-logo" />
-              <div>
-                <div className="org-position">
-                  Software Development Engineer - I
+          {carrerData.reverse().map((item) => {
+            return (
+              <li key={item.id} className="step-progress-item">
+                <div className="step-progress-item-content">
+                  <img src={item.orgLogo} className="org-logo" />
+                  <div>
+                    <div className="org-position">{item.orgPosition}</div>
+                    <a href={item.orgLink} target="_blank" className="org-name">
+                      {item.orgName}
+                    </a>
+                    <div className="org-tenure">{item.orgName}</div>
+                  </div>
                 </div>
-                <a
-                  href="https://skypointcloud.com"
-                  target="_blank"
-                  className="org-name"
-                >
-                  SkyPoint Cloud
-                </a>
-                <div className="org-tenure">June 2022 - Present</div>
-              </div>
-            </div>
-          </li>
-          <li className="step-progress-item">
-            <div className="step-progress-item-content">
-              <img src={OneBancLogo} className="org-logo" />
-              <div>
-                <div className="org-position">Frontend Developer</div>
-                <a
-                  href="https://onebanc.ai"
-                  target="_blank"
-                  className="org-name"
-                >
-                  OneBanc Technologies
-                </a>
-                <div className="org-tenure">Feb 2022 - June 2022</div>
-              </div>
-            </div>
-          </li>
-          <li className="step-progress-item">
-            <div className="step-progress-item-content">
-              <img src={OneBancLogo} className="org-logo" />
-              <div>
-                <div className="org-position">Associate Frontend Developer</div>
-                <a
-                  href="https://onebanc.ai"
-                  target="_blank"
-                  className="org-name"
-                >
-                  OneBanc Technologies
-                </a>
-                <div className="org-tenure">Oct 2021 - Feb 2022</div>
-              </div>
-            </div>
-          </li>
-          <li className="step-progress-item">
-            <div className="step-progress-item-content">
-              <img src={VitLogo} className="org-logo vit" />
-              <div>
-                <div className="org-position">Bachelor of Technology</div>
-                <a
-                  href="https://vit.ac.in"
-                  target="_blank"
-                  className="org-name"
-                >
-                  Vellore Institute of Technology
-                </a>
-                <div className="org-tenure">July 2017 - June 2021</div>
-              </div>
-            </div>
-          </li>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
